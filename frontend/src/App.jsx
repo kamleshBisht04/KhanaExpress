22f1b23dfeabca29fff5fa8293a1d01600aed726
kamleshBisht04 <kamleshbisht04@gmail.com>
2026-02-18 00:10:29 +0530

    display category section complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <div className="app">
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/cart" element={<Home/>} />
+        <Route path="/order" element={<Home/>} />
+      </Routes>
     </div>
   );
 }
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -2,35 +2,29 @@ import { assets } from "../../assets/assets";
 import "./Navbar.css";
 import { NavLink } from "react-router-dom";
 
+const navItems = [
+  { id: 1 ,title: "Home", target: "/" },
+  { id: 2 ,title: "menu", target: "/menu" },
+  { id: 3 ,title: "mobile-app", target: "/mobile-app" },
+  { id: 4 ,title: "contact us", target: "/contact-us" },
+];
+
 const Navbar = () => {
   return (
     <div className="navbar">
       <img src={assets.logo} className="logo" alt="khanaExpress logo " />
       <ul className="navbar-menu">
-        <li>
-          <NavLink to={"/home"} className={({ isActive }) => (isActive ? "active" : "")}>
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to={"/menu"} className={({ isActive }) => (isActive ? "active" : "")}>
-            menu
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to={"/mobile-app"} className={({ isActive }) => (isActive ? "active" : "")}>
-            mobile-app
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to={"/contact-us"} className={({ isActive }) => (isActive ? "active" : "")}>
-            contact us
-          </NavLink>
-        </li>
+        {navItems.map((item) => (
+          <li key={item.id}>
+            <NavLink to={item.target} className={({ isActive }) => (isActive ? "active" : "")}>
+              {item.title}
+            </NavLink>
+          </li>
+        ))}
       </ul>
 
       <div className="navbar-right">
-        <img src={assets.search_icon} className="navbar-search-icon" alt="search"  />
+        <img src={assets.search_icon} className="navbar-search-icon" alt="search" />
         <div className="navbar-search_icon">
           <img src={assets.basket_icon} alt="basket" />
           <div className="dot"></div>
