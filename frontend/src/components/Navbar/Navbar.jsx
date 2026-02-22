4bffecea110302769321f0b5936dd3139133d647
kamleshBisht04 <kamleshbisht04@gmail.com>
2026-02-22 17:04:49 +0530

    order page complete change the navbar shopping bag

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
-import { assets } from "../../assets/assets";
+// import { assets } from "../../assets/assets";
 import "./Navbar.css";
 import Logo from "../Logo/Logo";
 import { FiMenu, FiSearch, FiX } from "react-icons/fi";
+import { HiShoppingBag } from "react-icons/hi2";
 import useStore from "../../context/useStore";
 import { Link } from "react-router-dom";
 
@@ -14,9 +15,11 @@ const navItems = [
 ];
 
 const Navbar = () => {
-  const { search, setSearch, setShowLogin } = useStore();
+  const { search, setSearch, setShowLogin, cartItems } = useStore();
   const [activeMenu, setActiveMenu] = useState("home");
   const [isOpen, setIsOpen] = useState(false);
+
+  const totalItems = Object.values(cartItems).reduce((total, qty) => total + qty, 0);
 
   return (
     <div className="navbar">
@@ -63,9 +66,13 @@ const Navbar = () => {
 
         <div className="navbar-basket-icon">
           <Link to="/cart">
-            <img src={assets.basket_icon} alt="basket" />
+            <HiShoppingBag size={23}/>
+            {/* <img src={assets.basket_icon} alt="basket" /> */}
           </Link>
-          <div className="dot"></div>
+
+          {totalItems > 0 && (
+            <div className="cart-count">{totalItems > 99 ? "99+" : totalItems}</div>
+          )}
         </div>
 
         <button className="signin" onClick={() => setShowLogin(true)}>
